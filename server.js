f9ec8a1079a072171d9ad7f32ca39333128775a5
bentalla108 <tallambaye108@gmail.com>
2023-10-31 17:06:56 +0100

    Foodly backend for mobile app (Flutter / ReactNative)

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=> console.log('Connected to database Succesfully...'))
+    .catch((err) => console.log(err))
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(process.env.PORT || port, () => console.log(`Foodly server is running on port ${process.env.PORT }!`))
